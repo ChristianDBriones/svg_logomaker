09f219089b7b2d06a7ef09a8e45254d63078d2e9
ChristianDBriones <145032845+ChristianDBriones@users.noreply.github.com>
2024-01-12 17:39:03 -0600

    add neccessarry files and folders
    add neccesary files and folders

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log('hello world');
+
+
+// user selects text (up to 3 characters)
+
+
+
+
+// .gitignore
+
+// user selects colour
+// user selects shape
+
+
+/*
+import { input } from '@inquirer/prompts';
+
+const answer = await input({ message: 'Enter your name' });
+*/
