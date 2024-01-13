6365125ffd82b1d5069636f1a1c6676e4e2a0e8a
ChristianDBriones <145032845+ChristianDBriones@users.noreply.github.com>
2024-01-12 18:12:53 -0600

    add error
    add error if longer than 3 characters

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-console.log('hello world');
+//const input = require('@inquirer/prompts'); // CommonJS style import
+import { input } from '@inquirer/prompts';
+
+// await is only valid in async functions and the top level bodies of modules
+(async function() {
+  // Ask the user to enter the text of their logo
+  // Read it into a variable
+  const logoName2 = await input({ message: 'Enter the text of your logo' });
+  console.log(logoName2);
+  
+  // Make sure it's only 3 characters long
+  if (logoName2.length > 3)
+    console.error('Haha!');
+    //throw "Must be 3 or less characters";
+
+    // Package for generating an image
+    
+  //const answer = await input({ message: 'Enter your name' });
+  //console.log('hello ' + answer);
+})();
 
 
-// user selects text (up to 3 characters)
+// IIFE (Immeditately invoked function expression)
+
+// user selects text (up to 3 characters long)
 
 
 
@@ -13,7 +34,7 @@ console.log('hello world');
 
 
 /*
-import { input } from '@inquirer/prompts';
+import { input } from '@inquirer/prompts'; //ES6-style import
 
 const answer = await input({ message: 'Enter your name' });
 */
