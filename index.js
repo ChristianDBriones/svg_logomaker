d1ad210d9adf45797bbbd734886e438780cbc270
ChristianDBriones <145032845+ChristianDBriones@users.noreply.github.com>
2024-01-30 16:53:56 -0600

    add color, shape, text functionality

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import { input, select } from "@inquirer/prompts";
+import fs from 'fs';
+import path from 'path';
+import { Square, Circle, Triangle } from "./lib/shapes.js";
 
 // TODO: Include packages needed for this application
 
@@ -6,26 +9,54 @@ import { input, select } from "@inquirer/prompts";
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+  return fs.writeFile(path.join(process.cwd(), fileName), data, (err) =>
+    err ? console.error(err) : console.log('Success!'))
+}
 
 // TODO: Create a function to initialize app
-const logotext = await input({message: "what is the text of your logo?"});
-if(logotext.length > 3)
+const logotext = await input({ message: "what is the text of your logo?" });
+if (logotext.length > 3)
   console.log('can not be greater than 3 characters');
 
-const logoshape = await select({message: "what is the shape of your logo?", choices: [{
-  name: 'circle',
-  value: 'circle'
-},
-{
-  name: 'triangle',
-  value: 'triangle'
-},
-{
-  name: 'square',
-  value: 'square'
-},
-]});
+const logoshape = await select({
+  message: "what is the shape of your logo?", choices: [{
+    name: 'circle',
+    value: 'circle'
+  },
+  {
+    name: 'triangle',
+    value: 'triangle'
+  },
+  {
+    name: 'square',
+    value: 'square'
+  },
+  ]
+});
+const logocolor = await input({
+  message: "what is the color of your logo?"
+});
 
-console.log(logotext);
-console.log(logoshape);
+
+const logotextcolor = await input({
+  message: "what is the color is the text of your logo?"
+});
+
+if ("square" === logoshape) {
+  const square = new Square(logotextcolor, logotext, logocolor);
+  const svg = square.getSvg()
+  writeToFile("logo.svg", svg)
+}
+
+if ("triangle" === logoshape) {
+  const triangle = new Triangle(logotextcolor, logotext, logocolor);
+  const svg = square.getSvg()
+  writeToFile("logo.svg", svg)
+}
+
+if ("circle" === logoshape) {
+  const circle = new Circle(logotextcolor, logotext, logocolor);
+  const svg = circle.getSvg()
+  writeToFile("logo.svg", svg)
+}
